1eb70df5ddd970d155488a59cf861d8d40995eab
Shin Kojima <shin@kojima.org>
2021-08-10 17:08:15 +0900

    Care answers that MeCab don't know

--- content/ruby.py
+++ content/ruby.py
@@ -3,16 +3,38 @@ import MeCab
 import fileinput
 import regex as re
 import jaconv
+from warnings import warn
 
 tagger = MeCab.Tagger("-Ochasen")
 tagger.parse('')
+re_quote = re.compile(r'[『』]', re.UNICODE)
 re_last_ruby = re.compile(r'\(([\p{Hira}\p{Katakana}ー・]+)\)$', re.UNICODE)
 re_ruby = re.compile(r'\([^\)]*\)', re.UNICODE)
 re_not_wanted = re.compile(r'[^0-9A-Z\p{Katakana}ー]', re.UNICODE)
 
+extra_dict = {
+    '白樺派': 'シラカバハ',
+    '荀子': 'ジュンシ',
+    '靱猿': 'ウツボザル',
+    '隕石': 'インセキ',
+    '釆配': 'サイハイ',
+    '薀蓄': 'ウンチク',
+    '♯': 'シャープ',
+    '!': 'エクスクラメーションマーク',
+    '倖田來未': 'コウダクミ',
+    '靫猿': 'ウツボザル',
+    '鐚銭': 'ビタセン',
+    '發': 'ハツ',
+    '応永': 'オウエイ',
+    '鞐': 'コハゼ',
+    '姚明': 'ヤオミン',
+    '☆(星)': 'ホシ',
+}
+
 
 def prep(txt):
     raw = txt.split('※', 1)[0]
+    raw = re.sub(re_quote, '', raw)
 
     m = re_last_ruby.search(raw)
     if m:
@@ -24,6 +46,9 @@ def prep(txt):
 def to_kana(txt):
     prepared = prep(txt)
 
+    if prepared in extra_dict:
+        return extra_dict[prepared]
+
     node = tagger.parseToNode(prepared)
     words = []
     while node:
@@ -34,7 +59,12 @@ def to_kana(txt):
             words.append(jaconv.hira2kata(node.surface))
         node = node.next
 
-    return re.sub(re_not_wanted, '', ''.join(words).upper())
+    kana = re.sub(re_not_wanted, '', ''.join(words).upper())
+
+    if not kana:
+        warn(txt)
+
+    return kana
 
 
 for line in fileinput.input():
