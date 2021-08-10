26061582afe75bfd04fe78b95026f0564523973e
Hideo Yasumoto <hideo54@hideo54.com>
2021-08-10 17:15:24 +0900

    Temporarily change ws port

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,12 @@
 import WebSocket from 'ws';
 
-const wss = new WebSocket.Server({ host: '0.0.0.0', port: 8080 });
+const wss = new WebSocket.Server({ host: '0.0.0.0', port: 8081 });
 
 wss.on('connection', ws => {
     ws.send('Welcome!');
     ws.on('message', payload => {
         wss.clients.forEach(client => {
             client.send(payload.toString());
-        })
+        });
     });
 });
