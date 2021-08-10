576ad316cf105edb510c5eee75bc1fbb966f9203
Hideo Yasumoto <hideo54@hideo54.com>
2021-08-10 17:22:10 +0900

    Create a server to return problem randomly

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,20 @@
+import fastify from 'fastify';
+import { sample } from 'lodash';
+import fs from 'fs';
+
+const problems = fs.readFileSync('../content/quiz.tsv', 'utf-8').split('\n').map(line => {
+    const [ question, answer, answerInKana ] = line.split('\t');
+    return { question, answer, answerInKana };
+});
+
+const server = fastify();
+
+server.get('/problems/random', async () => {
+    return sample(problems);
+});
+
+server.listen(8000);
+
 import WebSocket from 'ws';
 
 const wss = new WebSocket.Server({ host: '0.0.0.0', port: 8081 });
