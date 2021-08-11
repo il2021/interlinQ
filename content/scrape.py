18f87a833b43686c4d4d7162853e0f7305efbf0d
Shin Kojima <shin@kojima.org>
2021-08-11 20:45:27 +0900

    Port scrape.pl into scrape.py

---
+++ content/scrape.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import requests
+from lxml import html
+
+BASE_URL = 'http://qss.quiz-island.site/abcgo/'
+
+for target in range(2003, 2014 + 1):
+    page = 1
+    total = None
+
+    while True:
+        response = requests.get(BASE_URL, params={
+            'formname': 'lite_search',
+            'target': target,
+            'page': page,
+        })
+
+        dom = html.fromstring(response.text)
+
+        for tr in dom.cssselect('#quizzes_list tr'):
+            question = tr.cssselect('td:nth-child(3) a')[0].text_content()
+            answer = tr.cssselect('td:nth-child(3) > div:nth-child(4)')[0].text_content()
+            answer = answer.removeprefix('正解 : ')
+            print("\t".join((question, answer)))
+
+        if total is None:
+            sel = '.pb-5 > p:nth-child(1) > strong:nth-child(1)'
+            total = int(dom.cssselect(sel)[0].text_content())
+
+        page += 1
+
+        if page * 100 < total:
+            break
