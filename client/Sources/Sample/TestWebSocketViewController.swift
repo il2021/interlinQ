7140e96df6c4d37096cd59b1118514c926e44d91
techiro <techiromen0@gmail.com>
2021-08-11 19:48:45 +0900

    add: ランダム問題のAPIを叩くコード

--- client/Sources/Sample/TestWebSocketViewController.swift
+++ client/Sources/Sample/TestWebSocketViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import SocketIO
-
 class TestWebSocketViewController: UIViewController {
 
     let manager = SocketManager(socketURL: URL(string:"http://localhost:8080/")!, config: [.log(true), .compress])
@@ -25,36 +24,65 @@ class TestWebSocketViewController: UIViewController {
             print("socket disconnected!")
         }
         
-        socket.on("from_server"){ data, ack in
+//        socket.on("from_server"){ data, ack in
+//            if let message = data as? [String] {
+//                print(message[0])
+//                self.dataList.insert(message[0],at: 0)
+//
+//            }
+//        }
+        
+        socket.on("responseQuestion") {data, ack in
             if let message = data as? [String] {
                 print(message[0])
-                self.dataList.insert(message[0],at: 0)
-                
             }
         }
+        
         socket.connect()
+        
+        
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         socket.disconnect()
     }
     @IBAction func tapButtonAction(_ sender: Any) {
-        socket.emit("from_client", CustomData(name: "Erik", age: 24)) {
-            print("送信完了")
-        }
+//        socket.emit("from_client", CustomData(name: "Erik", age: 24)) {
+//            print("送信完了")
+//        }
+        socket.emit("getQuestion")
+        
     }
     
     @IBAction func connectButtonAction(_ sender: Any) {
         socket.connect()
+        getRandomQuestion()
         
     }
     @IBAction func disconnectButtonAction(_ sender: Any) {
         socket.disconnect()
     }
     
-
 }
 
+func getRandomQuestion() {
+    let url: URL = URL(string: "http://localhost:8080/api/problems/random")!
+    let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
+        // コンソールに出力
+        do{
+            let couponData = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
+            print(couponData) // Jsonの中身を表示
+        }
+        catch {
+            print(error)
+        }
+        
+        print("data: \(String(describing: data))")
+        print("response: \(String(describing: response))")
+        print("error: \(String(describing: error))")
+    })
+    task.resume()
+}
 struct CustomData : SocketData {
     let name: String
     let age: Int
