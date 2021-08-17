7397f4bf571185c5dce791e2a9b104eda8243b0e
techiro <techiromen0@gmail.com>
2021-08-17 22:11:38 +0900

    add: HomeViewController

--- client/Sources/HomeViewController.swift
+++ client/Sources/HomeViewController.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    
+    var ActivityIndicator: UIActivityIndicatorView!
+    let viewModel = HomeViewModel()
+    var observers: [NSKeyValueObservation] = []
+    @IBOutlet weak var searchingText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchingText.text = ""
+        ActivityIndicator = UIActivityIndicatorView()
+        ActivityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        ActivityIndicator.center = self.view.center
+        
+        // クルクルをストップした時に非表示する
+        ActivityIndicator.hidesWhenStopped = true
+        
+        // 色を設定
+        ActivityIndicator.style = .medium
+        
+        //Viewに追加
+        self.view.addSubview(ActivityIndicator)
+    
+        let observer1 = viewModel.observe(\.labelText) { [weak self] (viewModel, _) in
+            self?.searchingText.text = viewModel.labelText
+        }
+        
+        
+        observers = [observer1]
     }
     
     
     @IBAction func tapGoButton(_ sender: Any) {
+        viewModel.isLoading ? ActivityIndicator.startAnimating() : ActivityIndicator.stopAnimating()
+        viewModel.buttonPressed()
+        
+    }
+    
+}
 
 class HomeViewModel: NSObject {
     @objc dynamic private(set) var labelText: String?
