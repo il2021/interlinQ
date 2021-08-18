48e7490f0cd0291cf3468ec1e8676c18090cf978
Shin Kojima <shin@kojima.org>
2021-08-18 18:19:24 +0900

    review: Reading entire lines may consumes a lot of memory

--- content/quiz_http_status_codes.py
+++ content/quiz_http_status_codes.py
@@ -3,43 +3,21 @@
 import csv
 
 Data_input = []
+input_file = "http-status-codes-1.csv"
+output_file = "quiz_http_status_codes.tsv"
 
-with open("http-status-codes-1.csv", encoding="utf-8", newline="") as f:
-    for cols in csv.reader(f):
-        Data_input.append(cols)
+with open(input_file, newline="\r\n") as input_f:
+    with open(output_file, "w", newline="") as output_f:
+        tsv = csv.writer(output_f, delimiter="\t", lineterminator="\n")
 
-ID = []
-Problems = []
-Answers = []
-Answers_kana = []
+        for i, row in enumerate(csv.DictReader(input_f), start=1):
+            if row["Description"] == "Unassigned":
+                continue
 
+            qid = "http-status-codes" + str(i)
+            problem = "「%s」を表すHTTPのステータスコードは何か" % (row["Description"],)
+            answer = row["Value"]
+            answer_kana = answer
+            print(problem, answer)
 
-for i, sentence in enumerate(Data_input):
-    if sentence[1] == "Unassigned" or sentence[1] == "Description":
-        continue
-
-    id = "http-status-codes" + str(i)
-    problem = "「" + sentence[1] + "」" + "を表すHTTPのステータスコードは何か"
-    answer = sentence[0]
-    answer_kana = sentence[0]
-
-    ID.append(id)
-    Problems.append(problem)
-    Answers.append(answer)
-    Answers_kana.append(answer_kana)
-    print(problem, answer)
-
-
-Data_output = []
-
-for i in range(len(Answers)):
-    id = ID[i]
-    problem = Problems[i]
-    answer = Answers[i]
-    answer_kana = Answers_kana[i]
-    string = id + "\t" + problem + "\t" + answer + "\t" + answer_kana
-    Data_output.append([string])
-
-with open("quiz_http_status_codes.tsv", "w", encoding="utf_8", newline="\n") as f:
-    writer = csv.writer(f, lineterminator="\n")
-    writer.writerows(Data_output)
+            tsv.writerow([qid, problem, answer, answer_kana])
