b9dd0ccb3dabb0366ac2de0870094706573d02b8
Hideo Yasumoto <hideo54@hideo54.com>
2021-08-18 09:40:16 +0900

    Add eslint and fix

--- browser-client/src/App.tsx
+++ browser-client/src/App.tsx
@@ -1,26 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
