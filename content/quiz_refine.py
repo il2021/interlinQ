e335adc29bf702b74b76d76652c91cea122eee45
Shin Kojima <shin@kojima.org>
2021-08-18 18:48:30 +0900

    review: Don't parse tsv yourself, let csv.reader to recognize delimiter

--- content/quiz_refine.py
+++ content/quiz_refine.py
@@ -2,41 +2,21 @@
 
 import csv
 
-Data_input = []
-
-with open("quiz.tsv", encoding="utf-8", newline="") as f:
-    for row in csv.reader(f, delimiter="\t"):
-        Data_input.append(row)
-
-ID = []
-Problems = []
-Answers = []
-Answers_kana = []
+input_file = "quiz.tsv"
+output_file = "quiz_refine.tsv"
 
 NG_Words = ["今年", "去年"]
 
-for id, problem, answer, answer_kana in Data_input:
-    if answer == "":
-        continue
-    if any(word in problem for word in NG_Words):
-        continue
-    ID.append(id)
-    Problems.append(problem)
-    Answers.append(answer)
-    Answers_kana.append(answer_kana)
-    print(id, answer)
+with open(input_file, newline="") as input_f:
+    with open(output_file, "w", encoding="utf_8_sig") as output_f:
+        tsv = csv.writer(output_f, delimiter="\t", lineterminator="\r\n")
 
+        for id, problem, answer, answer_kana in csv.reader(input_f, delimiter="\t"):
+            if answer == "":
+                continue
+            if any(word in problem for word in NG_Words):
+                continue
 
-Data_output = []
+            print(id, answer)
 
-for i in range(len(Answers)):
-    id = ID[i]
-    problem = Problems[i]
-    answer = Answers[i]
-    answer_kana = Answers_kana[i]
-    string = id + "\t" + problem + "\t" + answer + "\t" + answer_kana
-    Data_output.append([string])
-
-with open("quiz_refine.tsv", "w", encoding="utf_8_sig", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(Data_output)
+            tsv.writerow([id, problem, answer, answer_kana])
