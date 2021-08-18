2039ddda3c3fc3fdb7a3f41ec24ec5e3be452c51
Shin Kojima <shin@kojima.org>
2021-08-18 18:53:24 +0900

    review: Use snake_case

--- content/quiz_refine.py
+++ content/quiz_refine.py
@@ -5,7 +5,7 @@ import csv
 input_file = "quiz.tsv"
 output_file = "quiz_refine.tsv"
 
-NG_Words = ["今年", "去年"]
+ng_words = ["今年", "去年"]
 
 with open(input_file, newline="") as input_f:
     with open(output_file, "w") as output_f:
@@ -14,7 +14,7 @@ with open(input_file, newline="") as input_f:
         for qid, problem, answer, answer_kana in csv.reader(input_f, delimiter="\t"):
             if answer == "":
                 continue
-            if any(word in problem for word in NG_Words):
+            if any(word in problem for word in ng_words):
                 continue
 
             print(qid, answer)
