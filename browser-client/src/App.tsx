5fbeaa7f001dd564dc29123e74d9626932de7d2c
Hideo Yasumoto <hideo54@hideo54.com>
2021-08-18 10:06:45 +0900

    Create 入室 UI

--- browser-client/src/App.tsx
+++ browser-client/src/App.tsx
@@ -1,10 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+    const [userName, setUserName] = useState<string>('名無し');
+    const [status, setStatus] = useState<'waiting' | null>(null);
     return (
-        <p>Hello, world!</p>
+        <div style={{ textAlign: 'center' }}>
+            <h1>interlinQ Browser Client</h1>
+            <div>
+                status: {status || 'null'}
+            </div>
+            <div style={{ margin: '1em 0' }}>
+                <label htmlFor='userName'>ユーザー名: </label>
+                <input
+                    id='userName'
+                    value={userName}
+                    disabled={status !== null}
+                    onChange={e => setUserName(e.target.value)} />
+            </div>
+            <div>
+                {
+                    status === null && <button onClick={() => setStatus('waiting')}>入室する</button>
+                }
+            </div>
+        </div>
     );
 };
 
