bbb53d2755a5197716dfa10f70cc8be9ccd05661
Shin Kojima <shin@kojima.org>
2021-08-18 16:56:03 +0900

    Salvage content, server directory from merge commits

--- content/scrape_education.py
+++ content/scrape_education.py
@@ -14,8 +14,9 @@
 """
 
 import csv
-import re
-from collections import OrderedDict
+import random
+import time
+import unicodedata
 from urllib import request
 
 from bs4 import BeautifulSoup
@@ -23,28 +24,21 @@ from bs4 import BeautifulSoup
 
 class Scrape:
     def __init__(self):
-        self._dict = OrderedDict()
+        self.words = []
+        self.meaning = []
         self.url = ""
-        self.save_file_name = "quiz_education.csv"
-
-    # backward compat
-    @property
-    def words(self):
-        return list(self._dict.keys())
-
-    # backward compat
-    @property
-    def meaning(self):
-        return list(self._dict.values())
+        self.saveFileName = "quiz_education.csv"
 
     def _init(self):
-        with open(self.save_file_name, "w", encoding="utf-8") as f:
+        with open(self.saveFileName, "w", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerow(["**文部科学省のサイトより**"])
 
-    def _get_contents(self):
-        with request.urlopen(self.url) as resp:
-            self.soup = BeautifulSoup(resp, features="html.parser")
+    def _getContents(self):
+        self.res = request.urlopen(self.url)
+        self.soup = BeautifulSoup(self.res, features="html.parser")
+        self.res.close()
+        time.sleep(5.0 + random.random())
 
     """
     * 日本語が含まれるかどうか判定
@@ -52,52 +46,63 @@ class Scrape:
     * ひらがながふくまれていなければ全部英字（正式名称）であると仮定。
     """
 
-    def _contain_japaneses(self, string):
+    def _containJapaneses(self, string):
         hiragana = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをん"
-        re_hiragana = re.compile("[" + hiragana + "]", re.U)
-        return bool(re_hiragana.search(string))
+        for ch in string:
+            if ch in hiragana:
+                return True
+        return False
+
+    def _changeEnc(self, string, enc="utf-8"):
+        return string.encode(enc).decode(enc)
 
     def _save(self):
-        with open(self.save_file_name, "a", encoding="utf-8") as f:
+        with open(self.saveFileName, "a", encoding="utf-8") as f:
             writer = csv.writer(f)
-            for word, meaning in self._dict.items():
-                writer.writerow([meaning, word])
+            for i in range(len(self.words)):
+                writer.writerow([self.meaning[i], self.words[i]])
 
-    def _get_contents_in_ministry_of_education(self):
+    def _getContentsInMinistryOfEducation(self):
         url1 = "https://www.mext.go.jp/b_menu/shingi/gijyutu/gijyutu4/toushin/attach/1337927.htm"
         url2 = "https://www.mext.go.jp/b_menu/shingi/gijyutu/gijyutu2/suishin/attach/1332892.htm"
 
         self.url = url1
-        self._get_contents()
-        contents_main = self.soup.find(id="contentsMain")
-        for h4 in contents_main.find_all("h4"):
-            sibling = h4.find_next_sibling("p")
-            self._dict[h4.string] = sibling.string
+        self._getContents()
+        self.words = [
+            self._changeEnc(str(word.string))
+            for word in self.soup.find(id="contentsMain").find_all("h4")
+        ]
+        self.meaning = [
+            self._changeEnc(str(meaning.string))
+            for meaning in self.soup.find(id="contentsMain").find_all("p")
+        ]
+        self._save()
         # print(self.words)
         # print(self.meaning)
 
         self.url = url2
-        self._get_contents()
-        contents_main = self.soup.find(id="contentsMain")
-        for h2 in contents_main.find_all("h2", class_=None):
-            word = str(h2.string)[1:-1]
-            sibling = h2.find_next_sibling("p")
-            meaning = sibling.get_text(strip=True)
-
-            meaning_lst = meaning.split("。")
-            if self._contain_japaneses(meaning_lst[0]):
-                self._dict[word] = meaning
-            else:
-                # XXX: "。" will be disappeared
-                self._dict[word] = "".join(meaning_lst[1:])
-
+        self._getContents()
+        self.words = [
+            self._changeEnc(str(word.string))[1:-1]
+            for word in self.soup.find(id="contentsMain").find_all("h2")
+        ][:-1]
+        self.meaning = [
+            self._changeEnc(str(meaning.string))[1:]
+            for meaning in self.soup.find(id="contentsMain").find_all("p")
+        ][:-1]
+        for i in range(len(self.meaning)):
+            if not self._containJapaneses(str(self.meaning[i]).split("。")[0]):
+                tmp = str(self.meaning[i]).split("。")
+                self.meaning[i] = ""
+                for j in range(1, len(tmp)):
+                    self.meaning[i] += tmp[j]
         self._save()
         # print(self.words)
         # print(self.meaning)
 
     def main(self):
         self._init()
-        self._get_contents_in_ministry_of_education()
+        self._getContentsInMinistryOfEducation()
 
 
 if __name__ == "__main__":
