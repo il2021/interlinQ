460057937b09389975b8e7ee0a8338f4282d3627
EmiriKO <71414805+EmiriKO@users.noreply.github.com>
2021-08-19 10:59:21 +0900

    Add sound effect

--- client/Sources/ViewController.swift
+++ client/Sources/ViewController.swift
@@ -62,7 +62,7 @@ class ViewController: UIViewController {
             // Dispose of any resources that can be recreated.
         }
 
-    @IBAction func Buzzer(_ sender: Any) {
+    @IBAction func buzzer(_ sender: Any) {
         // 再生する音声ファイルを指定する
         let soundURL = Bundle.main.url(forResource: "Buzzer", withExtension: "mp3")
         do {
@@ -74,7 +74,39 @@ class ViewController: UIViewController {
         }
     }
 
-
+    @IBAction func correctAnswer(_ sender: Any) {
+        let soundURL = Bundle.main.url(forResource: "Correct_Answer", withExtension: "mp3")
+        do {
+            // 効果音を鳴らす
+            player = try AVAudioPlayer(contentsOf: soundURL!)
+            player?.play()
+        } catch {
+            print("error...")
+        }
+    }
+    
+    @IBAction func qustion(_ sender: Any) {
+        let soundURL = Bundle.main.url(forResource: "Question", withExtension: "mp3")
+        do {
+            // 効果音を鳴らす
+            player = try AVAudioPlayer(contentsOf: soundURL!)
+            player?.play()
+        } catch {
+            print("error...")
+        }
+    }
+    
+    @IBAction func wrongAnswer(_ sender: Any) {
+        let soundURL = Bundle.main.url(forResource: "Wrong_Answer", withExtension: "mp3")
+        do {
+            // 効果音を鳴らす
+            player = try AVAudioPlayer(contentsOf: soundURL!)
+            player?.play()
+        } catch {
+            print("error...")
+        }
+    }
+    
     //display sentence
     
     @IBAction func toTestButtonAction(_ sender: Any) {
